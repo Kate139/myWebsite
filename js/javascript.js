2d83fd745d88a577dc06f0764a508a82eeb4329d
Kate139 <katiewiggins823@gmail.com>
2025-05-21 01:44:29 -0700

    re-uploaded first draft

---
+++ js/javascript.js
@@ -0,0 +1,16 @@
+function goToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+// Expanding panels logic
+document.addEventListener("DOMContentLoaded", () => {
+  const panels = document.querySelectorAll('.panel');
+
+  panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+      panels.forEach(p => p.classList.remove('active'));
+      panel.classList.add('active');
+    });
+  });
+});
